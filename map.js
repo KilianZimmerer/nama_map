57621fd22ec2f1650fa434ddae36a4adf6f8850f
KilianZimmerer <kikleer@altlautern.de>
2020-03-29 17:54:05 +0200

    A first draft

---
+++ map.js
@@ -0,0 +1,14 @@
+// initialize the map
+var map = L.map('map', {zoomControl: false}).setView([52.511946, 13.406166], 14);
+var imgNotAvailable = 'https://www.postfreeadvertising.com/wp-content/uploads/2018/08/no-image-available-grid-263x172.png'
+var toBeUsedTile = 'https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
+// load a tile layer
+L.tileLayer(
+    toBeUsedTile,
+  {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 17,
+    minZoom: 5,
+    accessToken: 'pk.eyJ1IjoibXl1c2VybmFtZWlzdGhpcyIsImEiOiJjazhjcnBhaGIwM3EwM29wZ25xNDhyanc4In0.Z_35X_kOuFSvYRsMnf8QfA',
+  }
+).addTo(map);
---
+++ shop_data.js
@@ -0,0 +1,48 @@
+// sample data
+var shop_data = [
+    {
+    'company_name': 'Onion Shop',
+    'address': 'Sesamstraße 12',
+    'shop_url': 'http://78.47.131.16:3000/libraries/39',
+    'description': 'Hier gibt es die besten Zwiebeln in ganz Berlin.',
+    'logo': 'https://images-prod.healthline.com/hlcmsresource/images/AN_images/how-to-store-onions-1296x728-feature.jpg',
+    'latitude': 52.511946,
+    'longitude': 13.410166,
+    },
+    {
+    'company_name': 'Horse Shop',
+    'address': 'Sesamstraße 12',
+    'shop_url': 'http://78.47.131.16:3000/libraries/37',
+    'description': '',
+    'logo': 'https://i.ytimg.com/vi/JY2E1R6jo6k/maxresdefault.jpg',
+    'latitude': 52.511946,
+    'longitude': 13.400166,
+    },
+    {
+    'company_name': 'Honey Shop',
+    'address': 'Sesamstraße 12',
+    'shop_url': 'http://78.47.131.16:3000/libraries/47',
+    'description': 'Mjami Honey',
+    'logo': 'http://cdn.zmescience.com/wp-content/uploads/2013/04/bee.jpg',
+    'latitude': 52.517946,
+    'longitude': 13.406166,
+    },
+    {
+    'company_name': 'Färbmittelladen',
+    'address': 'Sesamstraße 12',
+    'shop_url': '',
+    'description': 'Färbemittel für alles.',
+    'logo': '',
+    'latitude': 52.508946,
+    'longitude': 13.402166,
+    },
+    {
+    'company_name': 'Spielsachen',
+    'address': 'Sesamstraße 12',
+    'shop_url': '',
+    'description': '',
+    'logo': '',
+    'latitude': 52.512946,
+    'longitude': 13.408166,
+    }
+  ];
