a510eaf1ae775eda6a83e521413c2b0b5ead4ba2
KilianZimmerer <kikleer@altlautern.de>
2020-03-30 16:36:09 +0200

    change zoom to reduce tile requests

--- map.js
+++ map.js
@@ -2,14 +2,16 @@
 var map = L.map('map', {zoomControl: true}).setView([52.511946, 13.406166], 14);
 var imgNotAvailable = 'https://www.postfreeadvertising.com/wp-content/uploads/2018/08/no-image-available-grid-263x172.png'
 var toBeUsedTile = 'https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
+var stadiamaps = 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}.png'
+var osmMap = 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png'
 // load a tile layer
 L.tileLayer(
-    toBeUsedTile,
+  toBeUsedTile,
   {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 17,
-    minZoom: 5,
-    accessToken: 'pk.eyJ1IjoibXl1c2VybmFtZWlzdGhpcyIsImEiOiJjazhjcnBhaGIwM3EwM29wZ25xNDhyanc4In0.Z_35X_kOuFSvYRsMnf8QfA',
+    minZoom: 11,
+    accessToken: 123,
     tileSize: 512,
     zoomOffset: -1
   }
