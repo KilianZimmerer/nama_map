82715159bb7f5abad798bcf067d695791ae49f6b
KilianZimmerer <kikleer@altlautern.de>
2020-03-31 22:02:03 +0200

    add address field and php

---
+++ submit_registration.php
@@ -0,0 +1,44 @@
+<html>
+<body>
+
+Shop Name: <?php echo $_POST["shop_name"]; ?><br>
+Email: <?php echo $_POST["shop_email"]; ?>
+Telefon: <?php echo $_POST["shop_phone"]; ?>
+Erreichbarkeit: von <?php echo $_POST["phone_availability_from"]; ?> bis <?php echo $_POST["phone_availability_until"]; ?>
+Beschreibung: <?php echo $_POST["shop_description"]; ?>
+Webseite: <?php echo $_POST["shop_website"]; ?>
+Kategorien: <?php echo $_POST["shop_categories"]; ?>
+
+
+<?php
+   if(isset($_FILES['image'])){
+      $errors= array();
+      $file_name = $_FILES['image']['name'];
+      $file_size =$_FILES['image']['size'];
+      $file_tmp =$_FILES['image']['tmp_name'];
+      $file_type=$_FILES['image']['type'];
+      $file_ext=strtolower(end(explode('.',$_FILES['image']['name'])));
+      
+      $extensions= array("jpeg","jpg","png");
+      
+      if(in_array($file_ext,$extensions)=== false){
+         $errors[]="extension not allowed, please choose a JPEG or PNG file.";
+      }
+      
+      if($file_size > 2097152){
+         $errors[]='File size must be excately 2 MB';
+      }
+      
+      if(empty($errors)==true){
+         move_uploaded_file($file_tmp,"images/".$file_name);
+         echo "Success";
+      }else{
+         print_r($errors);
+      }
+   }
+?>
+
+
+
+</body>
+</html>
