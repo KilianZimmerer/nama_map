b15f55da7a7e49afb78d398c18e35794466a7941
KilianZimmerer <kikleer@altlautern.de>
2020-04-03 17:33:44 +0200

    add individual map style (#40)

--- public/create_map.js
+++ public/create_map.js
@@ -2,12 +2,13 @@
 var map = L.map('map', { zoomControl: true }).setView([52.511946, 13.406166], 12);
 var imgNotAvailable = 'https://www.postfreeadvertising.com/wp-content/uploads/2018/08/no-image-available-grid-263x172.png'
 var toBeUsedTile = 'https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
+var myStyle = 'https://api.mapbox.com/styles/v1/myusernameisthis/ck8k8w25g02d01jmnnbehc46d/tiles/512/{z}/{x}/{y}?access_token={accessToken}'
 var stadiamaps = 'https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}.png'
 var osmMap = 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png'
 var cartoDB = 'https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png'
 // load a tile layer
 L.tileLayer(
-  toBeUsedTile,
+  myStyle,
   {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 16,
