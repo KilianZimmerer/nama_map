cbbcc492dbac0ca864e79e9045c805baef1ccba7
EmCeeEs <ab22uhrnachtunruhe@gmail.com>
2020-04-05 15:41:42 +0200

    --amend

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,13 @@
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
 
-var publicDir = 'dist';
+const PUBLIC_DIR = 'dist';
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.ts',
-  devtool: 'inline-source-map',
+  // devtool: 'inline-source-map',
   module: {
     rules: [
       {
@@ -26,16 +28,19 @@ module.exports = {
     ],
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js'],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, publicDir),
+    path: path.resolve(__dirname, PUBLIC_DIR),
   },
   devServer: {
-    contentBase: path.join(__dirname, publicDir),
+    contentBase: path.join(__dirname, PUBLIC_DIR),
     compress: true,
     port: 9000
   },
-  plugins: [new HtmlWebpackPlugin()],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin(),
+  ],
 };
