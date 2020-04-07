634c30ad06b594fe4431d82512670688d0d8d60b
KilianZimmerer <kikleer@altlautern.de>
2020-04-07 13:22:59 +0200

    change zoom level of start view to 11 (#53)

--- public/create_map.js
+++ public/create_map.js
@@ -1,5 +1,5 @@
 // initialize the map
-var map = L.map('map', { zoomControl: true }).setView([52.511946, 13.406166], 12);
+var map = L.map('map', { zoomControl: true }).setView([52.511946, 13.406166], 11);
 var imgNotAvailable = 'https://www.postfreeadvertising.com/wp-content/uploads/2018/08/no-image-available-grid-263x172.png'
 var toBeUsedTile = 'https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/{z}/{x}/{y}?access_token={accessToken}'
 var myStyle = 'https://api.mapbox.com/styles/v1/myusernameisthis/ck8k8w25g02d01jmnnbehc46d/tiles/512/{z}/{x}/{y}?access_token={accessToken}'
@@ -11,7 +11,7 @@ L.tileLayer(
   myStyle,
   {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 16,
+    maxZoom: 17,
     minZoom: 11,
     accessToken: 'pk.eyJ1IjoibXl1c2VybmFtZWlzdGhpcyIsImEiOiJjazhndmhzNmYwNHV3M3NzMDcwNGs1ZjZzIn0.2ZRug8MHanm-5yQd9ptfGg',
     tileSize: 512,
